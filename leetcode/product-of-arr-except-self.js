dac6fb3777ce66965a2a36d797e727201e8fc97d
Vansitha Ratnayake <vansitha1@gmail.com>
2024-01-24 11:02:59 +0530

    product of arr except self problem

---
+++ leetcode/product-of-arr-except-self.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const productExceptSelf = function(nums) {
+
+
+};
+
+
+const nums = [1,2,3,4];
+const r = productExceptSelf(nums)
+// console.log(r);
+
