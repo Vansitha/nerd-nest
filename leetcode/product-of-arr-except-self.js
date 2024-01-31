db34f313922f6fe9f5eb8024b62aeea004667384
Vansitha Ratnayake <vansitha1@gmail.com>
2024-01-31 12:50:03 +0530

    product-of-arr-except-self leetcode problem

--- leetcode/product-of-arr-except-self.js
+++ leetcode/product-of-arr-except-self.js
@@ -2,13 +2,22 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const productExceptSelf = function(nums) {
+const productExceptSelf = function (nums) {
+  let result = new Array(nums.length).fill(1);
 
-
+  let prefix = 1;
+  for (let i = 0; i < nums.length; i++) {
+    result[i] = prefix;
+    prefix *= nums[i];
+  }
+  let postfix = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    result[i] *= postfix;
+    postfix *= nums[i];
+  }
+  return result;
 };
 
-
-const nums = [1,2,3,4];
-const r = productExceptSelf(nums)
-// console.log(r);
-
+const nums = [1, 2, 3, 4];
+const r = productExceptSelf(nums);
+console.log(r);
